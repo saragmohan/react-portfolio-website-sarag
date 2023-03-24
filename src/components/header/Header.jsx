3f0c520d0aa710bf60d3741b5db90ec2918ba177
Sarag Mohan P <sarag.mohan@gmail.com>
2023-03-24 20:12:10 +0530

    react-portfoiliop-website: Components and css files added

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <Header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Sarag Mohan P</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </Header>
+  );
+}
+
+export default Header;
